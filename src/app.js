827882243ee97741c08c818dd5a8a4d4977590dd
Talha Faisal <talhafaisal2510@gmail.com>
2025-12-10 21:53:41 +0500

    Update / Allows large request bodies

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const seedAdmin = require("./seeder/adminSeeder");
 const errorHandler = require("./middlewares/errorHandler");
 const response = require("./utils/response");
 
-const allRoutes = require("./routes/index"); 
+const allRoutes = require("./routes/index");
 
 const app = express();
 app.use(morgan("dev"));
@@ -33,7 +33,8 @@ app.use(
   })
 );
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 
 // ------------------ Database Init ------------------
