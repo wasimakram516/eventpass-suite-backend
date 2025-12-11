5cd1c635df6bf01240313ed5740b03c7bba10d68
Talha Faisal <talhafaisal2510@gmail.com>
2025-12-11 19:09:29 +0500

    Deleted / upload fields because direct links will be provided

--- src/routes/quiznest/gameRoutes.js
+++ src/routes/quiznest/gameRoutes.js
@@ -2,23 +2,15 @@ const express = require("express");
 const router = express.Router();
 const gameController = require("../../controllers/quiznest/QNgameController");
 const { protect, adminOnly, checkPermission } = require("../../middlewares/auth");
-const upload = require("../../middlewares/uploadMiddleware");
 
 const quiznestAccess = [protect, checkPermission.quiznest];
 
-// Upload up to 3 images: cover, name, background
-const gameImageUpload = upload.fields([
-  { name: "cover", maxCount: 1 },
-  { name: "name", maxCount: 1 },
-  { name: "background", maxCount: 1 },
-]);
-
-router.post("/", quiznestAccess, gameImageUpload, gameController.createGame);
+router.post("/", quiznestAccess, gameController.createGame);
 router.get("/business/:slug", gameController.getGamesByBusinessSlug);
 router.get("/", quiznestAccess, gameController.getAllGames);
 router.get("/:id", quiznestAccess, gameController.getGameById);
 router.get("/slug/:slug", gameController.getGameBySlug);
-router.put("/:id", quiznestAccess, gameImageUpload, gameController.updateGame);
+router.put("/:id", quiznestAccess, gameController.updateGame);
 router.delete("/:id", quiznestAccess, gameController.deleteGame);
 
 module.exports = router;
