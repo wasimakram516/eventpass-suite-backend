1b5cd5956a51a8a67d72ba33f82a673cba52e120
Wasim Akram <mr.saim516@gmail.com>
2026-02-03 15:37:13 +0500

    Feature / Added SuperAdmin Role

--- src/models/User.js
+++ src/models/User.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema(
 
     role: {
       type: String,
-      enum: ["admin", "business", "staff"],
+      enum: ["superadmin", "admin", "business", "staff"],
       default: "business",
     },
     
